285d7575441b0b9aff102658471d9280650a4683
Alejandro Chavarria Cascante <alejandrochavarria667@gmail.com>
2022-07-21 22:48:33 -0600

    Comit

---
+++ TablaProducto.php
@@ -0,0 +1,49 @@
+<?php
+$conexion = mysqli_connect('localhost', 'root', '', 'inventario_tienda');
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+              rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Page Title</title>
+    </head>
+    <body>
+
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">idProducto</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad disponible</th>
+                        <th scope="col">Talla</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $sql = "SELECT * FROM producto";
+                    $result = mysqli_query($conexion, $sql);
+
+                    while ($mostrar = mysqli_fetch_array($result)) {
+                        ?>
+                        <tr>
+                            <th><?php echo $mostrar['idProducto'] ?></th>
+                            <th><?php echo $mostrar['descripcion'] ?></th>
+                            <th><?php echo $mostrar['precio'] ?></th>
+                            <th><?php echo $mostrar['cantDisponible'] ?></th>
+                            <th><?php echo $mostrar['talla'] ?></th>
+                        </tr>
+
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -8,5 +8,140 @@
         <title></title>
     </head>
     <body>
-    </body>
+
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <h2 class="nav-link"">Inventario</h2>
+            </li>
+        </li>
+    </ul>
+
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2331/2331716.png" class="card-img-top" alt="..." width="100px" height="200px"> 
+                    <div class="card-body">
+                        <h5 class="card-title">Producto</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a href='TablaProducto.php'>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3843/3843517.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Categoria</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1605/1605350.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Cliente</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2942/2942322.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Proveedor</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1819/1819145.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Paca</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1365/1365570.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Cajero</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/994/994649.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Venta</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/950/950258.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Detalle venta</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://cdn-icons-png.flaticon.com/512/4222/4222025.png" class="card-img-top" alt="..." width="100px" height="200px">>
+                    <div class="card-body">
+                        <h5 class="card-title">Movimientos</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted"><button><a>Ver datos</a></button></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+</body>
 </html>
